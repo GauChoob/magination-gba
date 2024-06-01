705480d7747f83b0234f078f4f12de5c56e6919c
GauChoob <starlight_tonight@hotmail.com>
2024-06-01 01:00:42 -0400

    Scene dumps

--- scene_render/magination_gba_mgba.lua
+++ scene_render/magination_gba_mgba.lua
@@ -14,7 +14,7 @@ Scene.__index = Scene
 function Scene.new()
     self = setmetatable({}, Scene)
     
-    self.address =  emu:read32(0x03000A38)
+    self.address = emu:read32(0x03000A38)
     if self.address == 0 then
         return self
     end
@@ -34,7 +34,9 @@ function Scene.new()
     return self
 end
 
-function Scene:save()
+function Scene:save(scene_id)
+    console:log(string.format('Saving ./scene_%04X.bin', scene_id))
+    console:log(string.format('SceneID: 0x%04X, Width: %d, Height: %d', self.scene_id, self.width, self.width))
     local f = io.open(string.format('./scene_%04X.bin', self.scene_id), 'wb')
     f:write(string.pack('<i', self.scene_id))
     f:write(string.pack('<i', self.width))
@@ -44,9 +46,82 @@ function Scene:save()
     f:write(self.tilemap)
     f:write(self.collmap)
     f:close()
-    console:log(string.format('Saved ./scene_%04X.bin',self.scene_id))
+end
+
+local scene_directories = {
+    [0] = true,
+    [68] = true,
+    [73] = true,
+    [129] = true,
+    [145] = true,
+    [185] = true,
+    [219] = true,
+    [227] = true,
+    [265] = true,
+    [271] = true,
+    [320] = true,
+    [341] = true,
+}
+local bad_scenes = {
+    [0x47] = true, -- instantly loads a different scene
+    -- The next 7 scenes don't exist in the scene directory
+    [0xC9] = true, -- crash
+    [0xF4] = true, -- crash
+    [0xF6] = true, -- crash
+    [0x156] = true, -- crash
+    [0x157] = true, -- crash
+    [0x158] = true, -- crash
+    [0x159] = true, -- crash
+}
+
+function next_scene()
+    -- Reset for next scene
+    scene_id = scene_id + 1
+    while scene_directories[scene_id] or bad_scenes[scene_id] do
+        -- skip scene directories as they don't actually contain a scene
+        -- skip problematic scenes
+        scene_id = scene_id + 1
+    end
+    emu:loadStateFile('scene_downloader.ss0')
+end
+
+function loop()
+    ticker = emu:read32(0x030016C0)
+    script_list = emu:read32(0x03003508)
+    script_scene = emu:read16(0x08186B0E)
+    current_scene = emu:read16(emu:read32(0x03000A38) + 0x0C)
+    buffer:clear()
+    buffer:print(string.format('Ticker: 0x%08X\n', ticker))
+    buffer:print(string.format('Target Scene: 0x%04X\n', scene_id))
+    buffer:print(string.format('Current Scene: 0x%04X\n', current_scene))
+    buffer:print(string.format('Script Scene ID: 0x%04X\n', script_scene))
+    buffer:print(string.format('Script Linked List: 0x%04X\n', script_list))
+    if scene_id >= scenes then
+        return
+    end
+    if ticker == 0x865 then
+        -- First frame of the save file
+        emu.memory.cart0:write16(0x186B0E, scene_id)
+    elseif (script_list == 0x300350C or ticker >= 0xA00) and scene_id == current_scene then
+        -- All scripts are done, dump the scene image
+        -- Or we time out after a few seconds because some scripts never end
+        scene = Scene.new()
+        scene:save(scene_id)
+        next_scene()
+    elseif ticker >= 0xB00 and scene_id ~= current_scene then
+        console:error(string.format('SKIPPING 0x%04X', scene_id))
+        next_scene()
+    end
 end
 
 
-scene = Scene.new()
-scene:save()
+console:log(string.format('Automatically saving dumps of all scenes'))
+buffer = console:createBuffer('info')
+scene_id = 0x185
+scenes = 398
+next_scene()
+callbacks:add('frame', loop)
+
+
+--scene = Scene.new()
+--scene:save()
--- scene_render/preview/render.py
+++ scene_render/preview/render.py
@@ -1,6 +1,26 @@
 import struct
 import os
 import png
+from collections import defaultdict
+
+
+def coll_reference(file):
+    width, height, pixels, metadata = png.Reader(file).read()
+    assert width == 128
+    assert height == 128
+    pixels = list(pixels)
+    if metadata['planes'] == 4:
+        pixels = [[color for i, color in enumerate(row) if i % 4 != 3] for row in pixels]  # Remove alpha channel
+    collset = []
+    for i in range(0x10):
+        for j in range(0x10):
+            colltile = [pixels[i*8 + y][j*8*3:j*8*3 + 8*3] for y in range(8)]
+            collset.append(colltile)
+    return collset
+
+
+collset = coll_reference('scene_render/Collision.png')
+interactset = coll_reference('scene_render/Hotspot.png')
 
 
 def process_color(word: int) -> tuple[int, int, int, int]:
@@ -8,7 +28,7 @@ def process_color(word: int) -> tuple[int, int, int, int]:
     g = (word & 0b1111100000) >> 5
     b = (word & 0b111110000000000) >> 10
     a = (word & 0b1000000000000000) >> 15
-    return r*8, g*8, b*8, 0 if a else 255
+    return r*8, g*8, b*8#, 0 if a else 255
 
 
 def process_palette(data: bytes) -> list[tuple[int, int, int, int]]:
@@ -37,14 +57,15 @@ class Tile:
         return f'{self.tileid:03X}'
 
 
-def process_tilemap(width: int, height: int, data: bytes):
+def process_tilemap(width: int, height: int, data: bytes) -> list[list[Tile]]:
     return [[Tile(data[2*(row*width + column)] + 256*data[2*(row*width + column) + 1]) for column in range(width)] for row in range(height)]
 
 
+# Represent the tileset by incrementing from 0x000 to 0x3FF
 tileset_tilemap = [[Tile(row*0x20 + column) for column in range(0x20)] for row in range(0x20)]
 
 
-def render_tilemap(tileset, tilemap, palette):
+def render_tilemap(tileset, tilemap, palette) -> list[list[int]]:
     height = len(tilemap)
     width = len(tilemap[0])
 
@@ -58,25 +79,118 @@ def render_tilemap(tileset, tilemap, palette):
     return ret
 
 
+class CollTile:
+    def __init__(self, collid, interactid):
+        self.collid = collid
+        self.interactid = interactid
+
+    def __repr__(self):
+        return f'{self.collid:02X}:{self.interactid:02X}'
+
+
+def process_collmap(width: int, height: int, data: bytes) -> list[list[CollTile]]:
+    return [[CollTile(data[2*(row*width + column)], data[2*(row*width + column) + 1]) for column in range(width)] for row in range(height)]
+
+
+def render_collmap(collmap) -> list[list[int]]:
+    height = len(collmap)
+    width = len(collmap[0])
+
+    def map_collset(row, column):
+        tile = collmap[row//8][column//8]
+        return collset[tile.collid][row % 8]
+
+    collids = [[map_collset(row, column) for column in range(0, width*8, 8)] for row in range(height*8)]
+    collids = [[num for pixel in row for num in pixel] for row in collids]  # Flatten
+
+    return collids
+
+
+def render_interactmap(collmap) -> list[list[int]]:
+    height = len(collmap)
+    width = len(collmap[0])
+    
+    def map_interactset(row, column):
+        tile = collmap[row//8][column//8]
+        return interactset[tile.interactid][row % 8]
+
+    interactids = [[map_interactset(row, column) for column in range(0, width*8, 8)] for row in range(height*8)]
+    interactids = [[num for pixel in row for num in pixel] for row in interactids]  # Flatten
+
+    return interactids
+
+
+def combine_collinteract(collids, interactids):
+    for i in range(len(collids)):
+        for j in range(len(collids[0])//3):
+            if interactids[i][j*3] + interactids[i][j*3 + 1] + interactids[i][j*3 + 2] == 255*3:
+                continue
+            collids[i][j*3] = collids[i][j*3]//2 + interactids[i][j*3]//2
+            collids[i][j*3 + 1] = collids[i][j*3 + 1]//2 + interactids[i][j*3 + 1]//2
+            collids[i][j*3 + 2] = collids[i][j*3 + 2]//2 + interactids[i][j*3 + 2]//2
+    return collids
+
 class SceneFile:
 
     def save_palette(self):
-        with open(self.get_path() + '.pal.png', 'wb') as f:
+        with open(self.get_path('pal'), 'wb') as f:
             w = png.Writer(width=16, height=16, bitdepth=8, palette=self.palette)
             w.write(f, [[16*j + i for i in range(16)] for j in range(16)])
 
     def save_tileset(self):
-        with open(self.get_path() + '.tileset.png', 'wb') as f:
-            w = png.Writer(width=256, height=256, alpha=True)
+        with open(self.get_path('tileset'), 'wb') as f:
+            w = png.Writer(width=256, height=256, alpha=False)
             w.write(f, render_tilemap(self.tileset, tileset_tilemap, self.palette))
 
     def save_tilemap(self):
-        with open(self.get_path() + '.map.png', 'wb') as f:
-            w = png.Writer(width=self.width*8, height=self.height*8, alpha=True)
-            w.write(f, render_tilemap(self.tileset, self.tilemap, self.palette))
+        with open(self.get_path('map'), 'wb') as f:
+            w = png.Writer(width=self.width*8, height=self.height*8, alpha=False)
+            self.rendered_tilemap = render_tilemap(self.tileset, self.tilemap, self.palette)
+            w.write(f, self.rendered_tilemap)
 
-    def get_path(self):
-        return f'temp/scene_{self.scene_id:04X}'
+    def save_collmap(self):
+        with open(self.get_path('coll'), 'wb') as f:
+            w = png.Writer(width=self.width*8, height=self.height*8, alpha=False)
+            self.rendered_collmap = render_collmap(self.collmap)
+            combined = [[sum(pixels)//2 for pixels in zip(*pairs)] for pairs in zip(self.rendered_tilemap, self.rendered_collmap)]
+            w.write(f, combined)
+
+    def save_interactmap(self):
+        with open(self.get_path('hot'), 'wb') as f:
+            w = png.Writer(width=self.width*8, height=self.height*8, alpha=False)
+            self.rendered_interactmap = render_interactmap(self.collmap)
+            combined = [[sum(pixels)//2 for pixels in zip(*pairs)] for pairs in zip(self.rendered_tilemap, self.rendered_interactmap)]
+            w.write(f, combined)
+    
+    def save_overlaymap(self):
+        with open(self.get_path('cmap'), 'wb') as f:
+            w = png.Writer(width=self.width*8, height=self.height*8, alpha=False)
+            combined = combine_collinteract(self.rendered_collmap, self.rendered_interactmap)
+            combined = [[sum(pixels)//2 for pixels in zip(*pairs)] for pairs in zip(self.rendered_tilemap, combined)]
+            w.write(f, combined)
+
+    def analyze_collmap(self):
+        collids = defaultdict(lambda: 0)
+        interactids = defaultdict(lambda: 0)
+        for row in self.collmap:
+            for tile in row:
+                collids[tile.collid] += 1
+                interactids[tile.interactid] += 1
+        def print_sorted(ids):
+            def sort_func(item):
+                return item[1]
+            sorted_list = sorted(ids.items(), key=sort_func, reverse=True)
+            for item in sorted_list:
+                print(f'0x{item[0]:02X}: {item[1]}')
+        print('Coll IDs')
+        print_sorted(collids)
+        print('Interact IDs')
+        print_sorted(interactids)
+
+
+    def get_path(self, extension):
+        os.makedirs(f'temp/{extension}/', exist_ok=True)
+        return f'temp/{extension}/scene_{self.scene_id:03}.{extension}.png'
 
     def make_folder(self):
         os.makedirs('temp/', exist_ok=True)
@@ -87,12 +201,18 @@ class SceneFile:
             self.palette: list[tuple[int, int, int, int]] = process_palette(f.read(0x200))
             self.tileset: list[list[int]] = process_tileset(f.read(0x8000))
             self.tilemap: list[list[Tile]] = process_tilemap(self.width, self.height, f.read(2*self.width*self.height))
-            self.collmap: list[list[Tile]] = process_tilemap(self.width, self.height, f.read(2*self.width*self.height))
+            self.collmap: list[list[CollTile]] = process_collmap(self.width, self.height, f.read(2*self.width*self.height))
         self.make_folder()
-        self.save_palette()
-        self.save_tileset()
+        #self.save_palette()
+        #self.save_tileset()
         self.save_tilemap()
-        #self.save_collmap()
+        #self.analyze_collmap()
+        self.save_collmap()
+        self.save_interactmap()
+        self.save_overlaymap()
 
-
-SceneFile('../mGBA-0.10.3-win64/scene_00F3.bin')
+for i in range(0x398):
+    file = f'scene_render/raw/scene_{i:04X}.bin'
+    if os.path.exists(file):
+        print(i)
+        SceneFile(file)
